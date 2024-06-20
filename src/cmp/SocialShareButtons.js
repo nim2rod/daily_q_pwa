ae0f382430657a50b1d64c1d4c15ca233abe4672
nimrod aviam <nimrod.a6@gmail.com>
2024-06-20 19:01:28 +0300

    handleShareOptions

--- src/cmp/SocialShareButtons.js
+++ src/cmp/SocialShareButtons.js
@@ -64,8 +64,7 @@ const SocialShareButtons = ({ url, text }) => {
                 <WhatsappShareButton url={url} title={text}>
                     <WhatsappIcon size={32} round={true}/>
                 </WhatsappShareButton>
-                {/* <p onClick={handleShareOptions}>•••</p> */}
-                <p>checkkkkkkk</p>
+                <p onClick={handleShareOptions}>•••</p>
             </div>
                 <Modal show ={showModal}/>
         </div>
