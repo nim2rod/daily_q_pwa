b1a2c112c90752de5a804f50a1748971e955b2c3
nimrod aviam <nimrod.a6@gmail.com>
2025-03-21 23:09:11 +0200

    submit button - try catch

--- src/cmp/SubmitButton.js
+++ src/cmp/SubmitButton.js
@@ -2,33 +2,31 @@ import React from 'react'
 import axios from 'axios'
 
 const SubmitButton = ({ code, setOutput, userId, setIsEditorFullScreen }) => {
-    const handleSubmit = () => {
+
+    const handleSubmit = async () => {
         setIsEditorFullScreen(false)
-        // axios.post('http://localhost:3030/submit-code', { code })
 
         if (userId === '') {
-            const randomId = (() => {
-                return Math.floor(Math.random() * 10000)
-            })
+            const randomId = () => Math.floor(Math.random() * 10000)
 
             let id = localStorage.getItem("tempId")
+
             if (!id) {
                 id = randomId()
                 localStorage.setItem("tempId", id)
             }
             userId = id
         }
-        console.log('userId: ', userId)
 
-        axios.post(`${process.env.REACT_APP_URL}/submit-code`, { code, userId })
-            .then(response => {
-                // console.log('response.data: ', response.data)
-                setOutput(response.data)
-            })
-            .catch(error => {
-                setOutput([`Error: ${error.message}`])
-            })
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_URL}/submit-code`, { code, userId })
+            setOutput(res.data)
+        } catch (error) {
+            console.error('error: ', error)
+            setOutput([`Error: ${error.message}`])
+        }
     }
+
     return (
         <button className='submit-btn' onClick={handleSubmit}>Submit</button>
     )
